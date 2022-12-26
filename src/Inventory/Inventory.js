74e527a11ee933322009b2b22a10f2167d126c00
Rakib Hasan <mrakibhasan199744@gmail.com>
2022-12-26 21:31:49 +0600

    React Roter set up

---
+++ src/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Inventory.css';
+const Inventory = () => {
+    return (
+        <div>
+            <h1>This is inventory</h1>
+        </div>
+    );
+};
+
+export default Inventory;
