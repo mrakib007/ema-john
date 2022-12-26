befcea3d960a1cd6547df03dcafe8366bf80d47a
Rakib Hasan <mrakibhasan199744@gmail.com>
2022-12-27 00:26:35 +0600

    cart custom hook added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import './Orders.css';
 
 const Orders = () => {
     const [products,setProducts] = useProducts();
+    const [cart,setCart] = useCart(products);
     console.log(products);
     return (
         <div>
             This is order: {products.length}
+            Cart has: {cart.length}
         </div>
     );
 };
