820dc60c1a092efff1093eb81ae3864f281d7fba
Rakib Hasan <mrakibhasan199744@gmail.com>
2022-12-27 00:12:42 +0600

    Custom hook created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Shop from './components/Shop/Shop';
 import {Routes,Route} from 'react-router-dom';
 import Orders from './components/Orders/Orders';
 import Inventory from './Inventory/Inventory';
+import About from './About/About';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   );
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 import './Orders.css';
 
 const Orders = () => {
+    const [products,setProducts] = useProducts();
+    console.log(products);
     return (
         <div>
-            This is order
+            This is order: {products.length}
         </div>
     );
 };
