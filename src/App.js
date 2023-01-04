3962de3eac09fb9af4ae908f8b8e3a82cb8c9a52
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-01-04 23:22:37 +0600

    shippment features added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './components/About/About';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import RequireAuth from './RequireAuth/RequireAuth';
+import Shipment from './components/Shipment/Shipment';
 
 
 function App() {
@@ -21,6 +22,11 @@ function App() {
         <Route path='/inventory' element={
           <RequireAuth>
             <Inventory></Inventory>
+          </RequireAuth>
+        }></Route>
+        <Route path='/shipment' element = {
+          <RequireAuth>
+            <Shipment></Shipment>
           </RequireAuth>
         }></Route>
         <Route path='/about' element={<About></About>}></Route>
