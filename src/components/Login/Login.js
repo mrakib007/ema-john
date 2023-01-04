84c313a8ebc3195d2974d031a74a49a6d9d2e42c
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-01-04 21:01:58 +0600

    routing issues solved

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,62 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  //   const [error, setError] = useState("");
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+  if (user) {
+    navigate("/shop");
+  }
+  const handleUserSignIn = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
   return (
     <div className="form-container">
       <div>
         <h2 className="form-title">Log In</h2>
-        <form action="">
+        <form action="" onSubmit={handleUserSignIn}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" required />
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" required />
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              required
+            />
           </div>
+          <p style={{ color: "red" }}>{error?.message}</p>
+          {loading && <p>Loading...</p>}
           <input className="form-submit" type="submit" value="Login" required />
         </form>
         <p>
-            New to Ema-John? <Link className="form-link" to="/signup">Create an account</Link>
+          New to Ema-John?{" "}
+          <Link className="form-link" to="/signup">
+            Create an account
+          </Link>
         </p>
       </div>
     </div>
